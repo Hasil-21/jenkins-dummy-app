96e9d45fa60d6594de38f8f4ebfb420e096d7a46
Hasil-21 <hasiltrivedi21@gmail.com>
2026-07-08 16:25:06 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Deploying dummy app..."
-echo "Version: 1.1"
+echo "Version: 1.2"
+echo "This is a pipleine for jenkins"
 echo "Webhook was added to this..."
 echo "Build timestamp: $(date)"
